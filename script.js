8af67f15d32f399cf2873e47824fbcceae5d1109
Douaa <douaachemnae@gmail.com>
2024-06-20 07:53:01 +0100

    berger menu is done

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu');
     const navbar = document.querySelector('.navbar');
-    // const nav = document.querySelector('.nav');
     menu.addEventListener('click', () => {
         navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
     });
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const menus = document.querySelector('.menus');
+        const navbars = document.querySelector('.navbars');
+    
+        menus.addEventListener('click', () => {
+            navbars.style.display = navbars.style.display === 'flex' ? 'none' : 'flex';
+        });
+    });
+    
+
 });
